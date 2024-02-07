5cf83c1e8122f29092f8d3c700a84e2d81b62253
Jude Idoko <judidoko@gmail.com>
2024-02-07 15:53:07 +0100

    Work on footer section and call Youtube api successfully

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import useSWR from "swr";
+
+import { fetchVideos } from "@/lib/api";
+
 export default function Home() {
+  // Hooks
+  const [badge, setBadge] = useState("All");
+  const {
+    data: videoResults,
+    error,
+    isLoading,
+  } = useSWR(`fetchVideo/${badge}`, () => fetchVideos("badge", 10));
+  console.log(videoResults);
   return (
     <>
       <div>Youtube-clone</div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import TopNavigation from "@/components/TopNavigation";
+import FooterMenu from "@/components/FooterMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         >
           <TopNavigation />
           <main className="pt-0"> {children}</main>
+          <FooterMenu />
         </ThemeProvider>
       </body>
     </html>
