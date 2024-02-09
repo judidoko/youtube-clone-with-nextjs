64db54f83f36f3e75d40d8b279b93025a8e20fa4
Jude Idoko <judidoko@gmail.com>
2024-02-09 16:33:30 +0100

    work on Search Badge component to enable filtering

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,9 +2,11 @@
 
 import { useState } from "react";
 import useSWR from "swr";
+import Loading from "../loading";
 
 import { fetchVideos } from "@/lib/api";
 import Thumbnail from "@/components/Thumbnail";
+import { SearchBadge } from "@/components/SearchBadge";
 
 export default function Home() {
   // Hooks
@@ -13,7 +15,7 @@ export default function Home() {
     data: videoResults,
     error,
     isLoading,
-  } = useSWR(`fetchVideo/${badge}`, () => fetchVideos("badge", 10));
+  } = useSWR(`fetchVideo/${badge}`, () => fetchVideos(badge, 12));
   // If there is error
   if (error) {
     throw new Error("Error fetching video data");
@@ -21,9 +23,17 @@ export default function Home() {
   return (
     <>
       <div className="fixed px-2 py-2 md:pl-[252px] top-16 left-0 w-screen z-20 dark:bg-black bg-white ">
-        Search Bagde
+        <SearchBadge
+          badges={["All", "JavaScript", "Reactjs", "Tailwind"]}
+          currentBadge={badge}
+          setBadge={setBadge}
+        />
       </div>
       <div className="flex flex-wrap">
+        {isLoading &&
+          Array(12)
+            .fill(null)
+            .map((i, index) => <Loading key={index} />)}
         {videoResults?.map((video) => (
           <Thumbnail key={video.id} video={video} />
         ))}
